fcfac371641718c22f6807fe0741e5bda090a353
Ronald Tanner <tar@semafor.ch>
2016-10-29 21:57:01 +0200

    various db connections added

--- esta-core/src/main/java/ch/semafor/esta/core/config/JpaConfig.java
+++ esta-core/src/main/java/ch/semafor/esta/core/config/JpaConfig.java
@@ -5,9 +5,13 @@ import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -21,15 +25,28 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan(basePackages={"ch.semafor.esta.core.service"})
 @EnableJpaRepositories("ch.semafor.esta.core.repository")
+@PropertySource(value = { "classpath:application.properties" })
 public class JpaConfig {
+	@Autowired
+	Environment env;
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer 
+	  propertySourcesPlaceholderConfigurer() {
+	    return new PropertySourcesPlaceholderConfigurer();
+	} 
+
 	@Bean
 	public DataSource dataSource() {
 	  DriverManagerDataSource dataSource = 
 	                       new DriverManagerDataSource();
-	  dataSource.setDriverClassName("org.postgresql.Driver");
-	  dataSource.setUrl("jdbc:postgresql://localhost/Esta");
-	  dataSource.setUsername("tar");
-	  dataSource.setPassword("");
+	  dataSource.setDriverClassName(env.getProperty(
+			  "hibernate.connection.driver_class"));
+	  dataSource.setUrl(env.getProperty(
+			  "hibernate.connection.url"));
+	  dataSource.setUsername(env.getProperty(
+			  "hibernate.connection.username"));
+	  dataSource.setPassword(env.getProperty(
+			  "hibernate.connnection.password"));
 	  return dataSource;
 	}
 	
@@ -46,10 +63,10 @@ public class JpaConfig {
 	           new HibernateJpaVendorAdapter();
 	  em.setJpaVendorAdapter(vendorAdapter);
 	  Properties properties = new Properties();
-	  properties.setProperty(
-	              "hibernate.hbm2ddl.auto", "create");
-	  properties.setProperty("hibernate.dialect",
-	              "org.hibernate.dialect.PostgreSQLDialect");
+	  properties.setProperty("hibernate.hbm2ddl.auto",
+			  env.getProperty("hibernate.hbm2ddl.auto"));
+//	  properties.setProperty("hibernate.dialect",
+//			  env.getProperty("hibernate.dialect"));
 
 	  em.setJpaProperties(properties);
 
@@ -59,11 +76,7 @@ public class JpaConfig {
 	@Bean
 	public PlatformTransactionManager transactionManager(
 				EntityManagerFactory emf) {
-	  JpaTransactionManager transactionManager = 
-	        new JpaTransactionManager();
-	  transactionManager.setEntityManagerFactory(emf);
-
-	  return transactionManager;
+		return new JpaTransactionManager(emf);
 	}
 	
 }
--- esta-core/src/main/java/ch/semafor/esta/core/service/StudentServiceImpl.java
+++ esta-core/src/main/java/ch/semafor/esta/core/service/StudentServiceImpl.java
@@ -1,13 +1,17 @@
 package ch.semafor.esta.core.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ch.semafor.esta.core.domain.Student;
 import ch.semafor.esta.core.repository.StudentRepository;
 
+@Service("studentService")
 @Transactional
 public class StudentServiceImpl implements StudentService{
 
+	@Autowired
 	StudentRepository studentRepository;
 	
 	@Override
